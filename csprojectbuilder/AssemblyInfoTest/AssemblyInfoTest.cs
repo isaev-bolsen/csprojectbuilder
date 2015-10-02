92c6e143d638c9253e49e06089d885a038f1be57
isaev-bolsen <al_mamistov@mail.ru>
2015-10-02 22:54:57 +0300

    AC-58452 : AssemblyInfo tests

---
+++ csprojectbuilder/AssemblyInfoTest/AssemblyInfoTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using csprojectbuilder;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+namespace AssemblyInfoTest
+{
+    [TestClass]
+    public class AssemblyInfoTest
+    {
+        private static IEnumerable<string> ReadLines(StreamReader reader)
+        {
+            while (!reader.EndOfStream) yield return reader.ReadLine();
+        }
+
+        [TestMethod]
+        public void VerifyAssemblyInfoLines()
+        {
+            AssemblyInfoCs AssInfo = new AssemblyInfoCs("MyAssembly", Guid.Empty);
+            Assert.AreEqual("MyAssembly", AssInfo.Product);
+            var currentLines = AssInfo.Lines.ToList();
+            var etallonLines = ReadLines(new FileInfo("AssemblyInfo.txt").OpenText()).
+                Where(L => !L.StartsWith("//") && !string.IsNullOrEmpty(L)).ToList();
+
+            Assert.AreEqual(etallonLines.Count, currentLines.Count);
+            for (int i = 0; i < etallonLines.Count; ++i)
+            {
+                Assert.AreEqual(etallonLines[i].Trim(), currentLines[i].Trim(), true);
+            }
+        }
+    }
+}
