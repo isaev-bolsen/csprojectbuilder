9afb968489822349fe8df819e821eb1d550a419b
isaev-bolsen <al_mamistov@mail.ru>
2015-10-07 22:21:01 +0300

    AC-58452  : some files to test

--- csprojectbuilder/AssemblyInfoTest/ProjectGroupTest.cs
+++ csprojectbuilder/AssemblyInfoTest/ProjectGroupTest.cs
@@ -12,6 +12,7 @@ namespace AssemblyInfoTest
         {
             ProjectGroup XMLCR = new ProjectGroup("MyAssembly", Guid.Empty);
             XMLCR.AddReferences("System", "System.Core", "System.Xml.Linq", "System.Data.DataSetExtensions", "Microsoft.CSharp", "System.Data", "System.Xml");
+            XMLCR.AddFiles(@"AP\AP101000_APSetupMaint.cs", @"AP\AP102000_APAccess.cs", @"AP\AP202000_APVendorPriceMaint.cs", @"AP\AP202010_APPriceWorksheetMaint.cs");
             string result = XMLCR.Result.ToString();
         }
     }
