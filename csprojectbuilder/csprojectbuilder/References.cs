936eebf10f5227a4669428fb17a63e2ad33e3078
isaev-bolsen <al_mamistov@mail.ru>
2015-10-07 20:04:50 +0300

    AC-58452 : References group

---
+++ csprojectbuilder/csprojectbuilder/References.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace csprojectbuilder
+{
+    class References
+    {
+        private List<XElement> Elements = new List<XElement>();
+
+        public XElement Element
+        {
+            get { return new XElement("ItemGroup", Elements); }
+        }
+
+        public void AddReference(string reference)
+        {
+            Elements.Add(new XElement("Reference", new XAttribute("Include", reference)));
+        }
+
+        public void AddReference(string reference, string HintPath)
+        {
+            Elements.Add(new XElement("Reference", new XAttribute("Include", reference), new XElement("HintPath", HintPath)));
+        }
+    }
+}
