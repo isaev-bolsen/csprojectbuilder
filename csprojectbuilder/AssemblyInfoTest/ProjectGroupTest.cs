8ff805619d7599a33c43a03f37a0d672f34369d2
isaev-bolsen <al_mamistov@mail.ru>
2015-10-07 21:59:10 +0300

    AC-58452  : add refernces in test

--- csprojectbuilder/AssemblyInfoTest/ProjectGroupTest.cs
+++ csprojectbuilder/AssemblyInfoTest/ProjectGroupTest.cs
@@ -11,7 +11,7 @@ namespace AssemblyInfoTest
         public void ProjectGroupTest1()
         {
             ProjectGroup XMLCR = new ProjectGroup("MyAssembly", Guid.Empty);
-
+            XMLCR.AddReferences("System", "System.Core", "System.Xml.Linq", "System.Data.DataSetExtensions", "Microsoft.CSharp", "System.Data", "System.Xml");
             string result = XMLCR.Result.ToString();
         }
     }
