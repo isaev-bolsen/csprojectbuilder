0ad525fee375c8c73f9b854de4f17b11f20dae44
isaev-bolsen <al_mamistov@mail.ru>
2015-10-07 20:41:32 +0300

    AC-58452 : adding simple references : namespace fix

--- csprojectbuilder/csprojectbuilder/References.cs
+++ csprojectbuilder/csprojectbuilder/References.cs
@@ -9,17 +9,17 @@ namespace csprojectbuilder
 
         public XElement Element
         {
-            get { return new XElement("ItemGroup", Elements); }
+            get { return new XElement(Utils.NameSpace + "ItemGroup", Elements); }
         }
 
         public void AddReference(string reference)
         {
-            Elements.Add(new XElement("Reference", new XAttribute("Include", reference)));
+            Elements.Add(new XElement(Utils.NameSpace + "Reference", new XAttribute("Include", reference)));
         }
 
         public void AddReference(string reference, string HintPath)
         {
-            Elements.Add(new XElement("Reference", new XAttribute("Include", reference), new XElement("HintPath", HintPath)));
+            Elements.Add(new XElement(Utils.NameSpace + "Reference", new XAttribute("Include", reference), new XElement(Utils.NameSpace + "HintPath", HintPath)));
         }
     }
 }
